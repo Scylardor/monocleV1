695383083722c230b6d1fb53009a832c292e6e31
Scylardor <baron.alx@gmail.com>
2017-05-09 22:30:54 +0200

    Add the "catch all" header moeLog.h, to include the whole Monocle logging system.

---
+++ MonocleSource/Core/Log/Include/moeLog.h
@@ -0,0 +1,23 @@
+#ifndef MOE_LOG_H_
+#define MOE_LOG_H_
+
+// This is the "include all things" header for Monocle logging.
+// It includes commonly used logger interfaces, logging utilities, log policies...
+// And the usually useful macros to log more easily.
+
+// The logger basically includes everything else
+#include "moeLogger.h"
+
+// Logger Policies
+#include "LogPolicies/NoFilterPolicy.h"
+#include "LogPolicies/SeverityFilterPolicy.h"
+
+#include "LogPolicies/NoFormatPolicy.h"
+#include "LogPolicies/DebuggerFormatPolicy.h"
+
+#include "LogPolicies/CaptureWritePolicy.h"
+#include "LogPolicies/FileWritePolicy.h"
+#include "LogPolicies/OutStreamWritePolicy.h"
+#include "LogPolicies/Windows/VSDebuggerWritePolicy.h"
+
+#endif // MOE_LOG_H_

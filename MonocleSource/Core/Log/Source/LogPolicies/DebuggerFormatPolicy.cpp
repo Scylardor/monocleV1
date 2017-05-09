bba24873b36af686bfe8d7baf39267cdf97253c2
Scylardor <baron.alx@gmail.com>
2017-05-09 22:29:15 +0200

    Add the DebuggerFormatPolicy logging format policy, meant to format log messages in the Visual Studio way.
    - Remarkably convenient because when used in VS, such messages take you to the given file/line when clicked upon

---
+++ MonocleSource/Core/Log/Source/LogPolicies/DebuggerFormatPolicy.cpp
@@ -0,0 +1,15 @@
+#include "LogPolicies/DebuggerFormatPolicy.h"
+#include "moeStringFormat.h"
+
+namespace moe
+{
+    void    DebuggerFormatPolicy::Format(std::string& buf, const LogInfo& logInfo)
+    {
+        StringFormat(buf, "%s(%d): [%s] (%s) %s",
+            logInfo.file,
+            logInfo.line,
+            internals::GetChannelString(logInfo.channel),
+            internals::GetSeverityString(logInfo.severity),
+            logInfo.message);
+    }
+}

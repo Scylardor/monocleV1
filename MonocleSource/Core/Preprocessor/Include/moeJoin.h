745d345591e7b22a2c07eec4bd50093a9bfd3132
Scylardor <baron.alx@gmail.com>
2017-05-12 20:10:48 +0200

    Add one more layer of indirection to MOE_JOIN for it to properly handle macro value parameters

--- MonocleSource/Core/Preprocessor/Include/moeJoin.h
+++ MonocleSource/Core/Preprocessor/Include/moeJoin.h
@@ -2,7 +2,9 @@
 #define MOE_JOIN_H_
 
 // This kind of strange idiom allows us to even join macro values together (e.g. __FILE__ ...)
-#define MOE_JOIN_INTERNAL(a, b) a ## b
+// Needs two (!) layers of indirection for macros to expand correctly in that case
+#define MOE_JOIN_INTERNAL2(a, b) a ## b
+#define MOE_JOIN_INTERNAL(a, b) MOE_JOIN_INTERNAL2(a, b)
 #define MOE_JOIN(a, b) MOE_JOIN_INTERNAL(a, b)
 
 #endif // MOE_JOIN_H_

c4e5b54e60e4388e75558a9a45f2d7d562d90927
Scylardor <baron.alx@gmail.com>
2017-06-03 20:22:31 +0200

    Add possibility to request VSync activation in context descriptor constructors. OpenGL contexts will be able to activate adaptive V-Sync, not D3D11 ones, those will fallback on simple v-sync.

--- MonocleSource/Graphics/Window/Include/moeContextDescriptor.h
+++ MonocleSource/Graphics/Window/Include/moeContextDescriptor.h
@@ -1,5 +1,5 @@
-#ifndef MOE_PIXEL_FORMAT_H_
-#define MOE_PIXEL_FORMAT_H_
+#ifndef MOE_CONTEXT_DESCRIPTOR_H_
+#define MOE_CONTEXT_DESCRIPTOR_H_
 
 #include "moeDLLVisibility.h"
 #include <cstdint>
@@ -16,37 +16,13 @@ namespace moe
 {
     struct MOE_DLL_API ContextDescriptor
     {
-        ContextDescriptor(std::uint8_t rgbaBits = DEFAULT_BITS_PER_COLOR, std::uint8_t depthBits = DEFAULT_DEPTH_BITS, std::uint8_t stencilBits = DEFAULT_STENCIL_BITS,
-            std::uint8_t numSamples = DEFAULT_SAMPLES_COUNT, std::int8_t apiMajorVersion = DEFAULT_API_MAJOR_VERSION, std::int8_t apiMinorVersion = DEFAULT_API_MINOR_VERSION,
-            bool hasDoubleBuffering = DEFAULT_DOUBLE_BUFFERING) :
-            DepthBits(depthBits),
-            StencilBits(stencilBits),
-            SamplesCount(numSamples),
-            API_MajorVersion(apiMajorVersion),
-            API_MinorVersion(apiMinorVersion),
-            UseDoubleBuffering(hasDoubleBuffering)
-        {
-            R = G = B = A = rgbaBits;
-        }
-
-        ContextDescriptor(std::uint8_t rgbBits, std::uint8_t alphaBits, std::uint8_t depthBits = DEFAULT_DEPTH_BITS, std::uint8_t stencilBits = DEFAULT_STENCIL_BITS,
-            std::uint8_t numSamples = DEFAULT_SAMPLES_COUNT, std::int8_t apiMajorVersion = DEFAULT_API_MAJOR_VERSION, std::int8_t apiMinorVersion = DEFAULT_API_MINOR_VERSION,
-            bool hasDoubleBuffering = DEFAULT_DOUBLE_BUFFERING) :
-            A(alphaBits),
-            DepthBits(depthBits),
-            StencilBits(stencilBits),
-            SamplesCount(numSamples),
-            API_MajorVersion(apiMajorVersion),
-            API_MinorVersion(apiMinorVersion),
-            UseDoubleBuffering(hasDoubleBuffering)
-        {
-            R = G = B = rgbBits;
-        }
-        ContextDescriptor(std::uint8_t rBits, std::uint8_t gBits, std::uint8_t bBits, std::uint8_t aBits,
-            std::uint8_t depthBits = DEFAULT_DEPTH_BITS, std::uint8_t stencilBits = DEFAULT_STENCIL_BITS,
-            std::uint8_t numSamples = DEFAULT_SAMPLES_COUNT,
-            std::int8_t apiMajorVersion = DEFAULT_API_MAJOR_VERSION, std::int8_t apiMinorVersion = DEFAULT_API_MINOR_VERSION,
-            bool hasDoubleBuffering = DEFAULT_DOUBLE_BUFFERING) :
+        // Fully-qualifying every parameter constructor. Delegates every other ctor
+        ContextDescriptor(
+            std::uint8_t rBits, std::uint8_t gBits, std::uint8_t bBits, std::uint8_t aBits,
+            std::uint8_t depthBits, std::uint8_t stencilBits,
+            std::uint8_t numSamples,
+            std::int8_t apiMajorVersion, std::int8_t apiMinorVersion,
+            bool hasDoubleBuffering, bool useVSync) :
             R(rBits),
             G(gBits),
             B(bBits),
@@ -56,8 +32,32 @@ namespace moe
             SamplesCount(numSamples),
             API_MajorVersion(apiMajorVersion),
             API_MinorVersion(apiMinorVersion),
-            UseDoubleBuffering(hasDoubleBuffering)
-        {}
+            UseDoubleBuffering(hasDoubleBuffering),
+            EnableVSync(useVSync)
+        {
+        }
+
+        // Version in which you specify the same number of bits for all R, G, B and A color channels.
+        ContextDescriptor(
+            std::uint8_t rgbaBits = DEFAULT_BITS_PER_COLOR,
+            std::uint8_t depthBits = DEFAULT_DEPTH_BITS, std::uint8_t stencilBits = DEFAULT_STENCIL_BITS,
+            std::uint8_t numSamples = DEFAULT_SAMPLES_COUNT,
+            std::int8_t apiMajorVersion = DEFAULT_API_MAJOR_VERSION, std::int8_t apiMinorVersion = DEFAULT_API_MINOR_VERSION,
+            bool hasDoubleBuffering = DEFAULT_DOUBLE_BUFFERING, bool useVSync = DEFAULT_USE_VSYNC) :
+            ContextDescriptor(rgbaBits, rgbaBits, rgbaBits, rgbaBits, depthBits, stencilBits, numSamples, apiMajorVersion, apiMinorVersion, hasDoubleBuffering, useVSync)
+        {
+        }
+
+        // Version in which you specify the same number of bits for all R, G, B color channels and a different value for Alpha.
+        ContextDescriptor(
+            std::uint8_t rgbBits, std::uint8_t alphaBits,
+            std::uint8_t depthBits = DEFAULT_DEPTH_BITS, std::uint8_t stencilBits = DEFAULT_STENCIL_BITS,
+            std::uint8_t numSamples = DEFAULT_SAMPLES_COUNT,
+            std::int8_t apiMajorVersion = DEFAULT_API_MAJOR_VERSION, std::int8_t apiMinorVersion = DEFAULT_API_MINOR_VERSION,
+            bool hasDoubleBuffering = DEFAULT_DOUBLE_BUFFERING, bool useVSync = DEFAULT_USE_VSYNC) :
+            ContextDescriptor(rgbBits, rgbBits, rgbBits, alphaBits, depthBits, stencilBits, numSamples, apiMajorVersion, apiMinorVersion, hasDoubleBuffering, useVSync)
+        {
+        }
 
         // Returns total number of bits used by a color pixel
         std::uint8_t    ColorBits() const
@@ -75,6 +75,7 @@ namespace moe
         std::int8_t     API_MajorVersion;
         std::int8_t     API_MinorVersion;
         bool            UseDoubleBuffering;
+        bool            EnableVSync;
 
 
         // Default are quite arbitrary, based on the most commonly used values for these usages...
@@ -85,7 +86,8 @@ namespace moe
         static const std::int8_t    DEFAULT_API_MAJOR_VERSION   = -1;
         static const std::int8_t    DEFAULT_API_MINOR_VERSION   = -1;
         static const std::uint8_t   DEFAULT_DOUBLE_BUFFERING    = true;
+        static const std::uint8_t   DEFAULT_USE_VSYNC           = false;
     };
 }
 
-#endif // MOE_PIXEL_FORMAT_H_
+#endif // MOE_CONTEXT_DESCRIPTOR_H_

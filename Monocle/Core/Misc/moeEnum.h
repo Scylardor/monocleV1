811e6f7a8794082508a3ce6571288275e7274ece
Alexandre Baron <baron.alx@gmail.com>
2019-06-10 00:26:37 +0200

    Add operator++/-- support for MOE_ENUM

--- Monocle/Core/Misc/moeEnum.h
+++ Monocle/Core/Misc/moeEnum.h
@@ -10,6 +10,15 @@
 	The goal is to provide a friendly and improved enum interface that allows easy enum iteration, stringization, etc.
 	I also added basic operator overloading to allow enums to be used as bitflags.
 
+	/!\ IMPORTANT THINGS TO KNOW:
+
+	- Better Enums can't be used inside the scope of a class, so MOE_ENUMs lack this feature too. You have to declare them in a namespace (or global scope).
+
+	- You can't use operators != or == like this : "if (myEnum == EnumType::EnumValue)" as of today. You have to do : "if (myEnum == +EnumType::EnumValue)".
+
+	- Better Enums doesn't support the "trailing comma syntax" standard enums provide (i.e. ending the enum list with a comma).
+
+
 	Note : if you read the http://blog.bitwigglers.org post you'll realize I didn't implement the SFINAE for bitflags enums.
 	It's because I thought it was too much added complexity for not a lot of gained value.
 	Instead, I use DECLARE_MOE_ENUM_OPERATORS and DEFINE_MOE_ENUM_OPERATORS macros. When creating a new MOE_ENUM, you need to use both
@@ -26,13 +35,17 @@
 BETTER_ENUM(Enum, UnderlyingType, ##__VA_ARGS__);
 
 #define DECLARE_MOE_ENUM_OPERATORS(Enum)\
-Enum operator |(Enum lhs, Enum rhs);\
-Enum operator &(Enum lhs, Enum rhs);\
-Enum operator ^(Enum lhs, Enum rhs);\
-Enum operator ~(Enum rhs);\
-Enum& operator |=(Enum &lhs, Enum rhs);\
-Enum& operator &=(Enum &lhs, Enum rhs);\
-Enum& operator ^=(Enum &lhs, Enum rhs);
+Enum	operator |(Enum lhs, Enum rhs);\
+Enum	operator &(Enum lhs, Enum rhs);\
+Enum	operator ^(Enum lhs, Enum rhs);\
+Enum	operator ~(Enum rhs);\
+Enum&	operator |=(Enum &lhs, Enum rhs);\
+Enum&	operator &=(Enum &lhs, Enum rhs);\
+Enum&	operator ^=(Enum &lhs, Enum rhs); \
+Enum&	operator ++(Enum& rhs); /* prefix ++ */ \
+Enum	operator ++(Enum& lhs, int); /* postfix ++ */ \
+Enum&	operator --(Enum& rhs); /* prefix -- */ \
+Enum	operator --(Enum& lhs, int); /* postfix -- */
 
 
 #define DEFINE_MOE_ENUM_OPERATORS(Enum) \
@@ -74,4 +87,28 @@ Enum& operator ^=(Enum &lhs, Enum rhs) \
 { \
 	lhs = lhs ^ rhs; \
 	return lhs; \
+} \
+Enum& operator ++(Enum& rhs) /* prefix ++ */ \
+{ \
+	rhs = Enum::_from_integral(rhs._to_integral() + 1); \
+	return rhs; \
+} \
+Enum operator ++(Enum& lhs, int) /* postfix ++ */ \
+{ \
+	Enum copy = lhs; \
+	lhs = Enum::_from_integral(lhs._to_integral() + 1); \
+	return copy; \
+} \
+Enum& operator --(Enum& rhs) /* prefix -- */ \
+{ \
+	rhs = Enum::_from_integral(rhs._to_integral() - 1); \
+	return rhs; \
+} \
+Enum operator --(Enum& lhs, int) /* postfix -- */ \
+{ \
+	Enum copy = lhs; \
+	lhs = Enum::_from_integral(lhs._to_integral() - 1); \
+	return copy; \
 }
+
+
